41b3c9c95e876f8ad795ab2949eb6308727247ca
Alice <alice.millour@abtela.eu>
2020-12-18 00:08:24 +0100

    update assets links
    Former-commit-id: d13a2d6be4e629b4b20c1002b8a6c2010c355cd7

---
+++ assets/pdfs/models/2019_100_full_gsw_PROPN/train-command.sh
@@ -0,0 +1,1 @@
+./MElt-train 2019_100_full_gsw_PROPN ../lexicons/lexicon_full_gsw_PROPN.txt ../training_corpus/size/2019_100.txt ../test_corpus/brown/original_eval.csv
---
+++ assets/pdfs/models/2019_all_we_got_lexique_gsw_PROPN/train-command.sh
@@ -0,0 +1,1 @@
+./MElt-train 2019_all_we_got_lexique_gsw_PROPN ../lexicons/lexicon_full_gsw_PROPN.txt ../training_corpus/2019_all_we_got.csv ../test_corpus/brown/original_eval.csv
